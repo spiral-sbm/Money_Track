1fc6e52c0f3689c1a0a7196e41a31077d3d383b3
Jmspter <jamesideias@gmail.com>
2024-07-12 20:05:13 -0300

    categorias

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :categories
   has_many :bank_accounts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   authenticated :user do
     root to: 'index#dashboard', as: :authenticated_root
   end
-
   unauthenticated do
     root to: 'index#welcome', as: :unauthenticated_root
   end
 
+  resources :categories, except: [:index, :show]
   resources :bank_accounts, except: [:index, :show]
 end
